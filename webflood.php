324609a4159226a4fceb4c22b2d5dd4f911d727a
Sheikh-Nightshader <gillesderaisboy@gmail.com>
2024-10-03 17:39:24 -0400

    Update webflood.php

--- webflood.php
+++ webflood.php
@@ -103,7 +103,7 @@
                         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
                     ];
 
-                    // Example payloads
+                    
                     $payloads = [
                         "payload1=value1",
                         "payload2=value2",
